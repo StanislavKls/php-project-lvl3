08fe14d0c787b5575a8c8f35fa3e273ee5ca46e2
StanislavKls <73436574+StanislavKls@users.noreply.github.com>
2021-04-01 11:59:28 +0500

    fix lint

--- app/Http/Controllers/SiteCheckController.php
+++ app/Http/Controllers/SiteCheckController.php
@@ -8,80 +8,15 @@ use Illuminate\Support\Facades\DB;
 class SiteCheckController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Request $request, $id, $check_id)
+    public function show(Request $request, $id, $check_id): \Illuminate\View\View
     {
-        $url =   DB::table('urls')->find($id);
+        $url   = DB::table('urls')->find($id);
         $check = DB::table('url_checks')->find($check_id);
         return view('urls.checks', compact('url', 'check'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
